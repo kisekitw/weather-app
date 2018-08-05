a0caf4f1bc644872ba36178865749c977138eb35
kisekitw <kisekitw@gmail.com>
2018-08-05 22:59:57 +0800

    Writing Up Weather Search

--- app.js
+++ app.js
@@ -2,12 +2,48 @@ const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
 
+// const argv = yargs
+//             .option({
+//                 a: {
+//                     demand: true,
+//                     alias: 'address',
+//                     description: 'Address of fetch weather for',
+//                     string: true
+//                 }
+//             })
+//             .help()
+//             .alias('h', 'help')
+//             .argv;
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+// const yargs = require('yargs');
+const request = require('request');
+
 const argv = yargs
-            .option({
-                a: {
+            .options({
+                k: {
                     demand: true,
-                    alias: 'address',
-                    description: 'Address of fetch weather for',
+                    alias: 'key',
+                    description: 'User key',
+                    string: true
+                },
+                lat: {
+                    demand: true,
+                    alias: 'latitude',
+                    description: 'latitude of location',
+                    string: true
+                },
+                lon: {
+                    demand: true,
+                    alias: 'longitude',
+                    description: 'longitude of location',
                     string: true
                 }
             })
@@ -15,12 +51,16 @@ const argv = yargs
             .alias('h', 'help')
             .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+geocode.getTemperature(argv, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage);
     } else {
         console.log(JSON.stringify(results, undefined, 2));
     }
-    
 });
+
+// key:bc4f2ab49f522007e2f65eebf9d48bc2
+// "latitude": 22.6624845,
+// "longitude": 120.2890786
+// https://api.darksky.net/forecast/[key]/[latitude],[longitude]
 
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -7,7 +7,6 @@ var geocodeAddress = (originalAddress, callback) => {
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
         json: true
     }, (error, response, body) => {
-    
         if (error) {
             callback('Unable to connect to google server.');
         } else if (body.status === 'ZERO_RESULTS') {
@@ -22,6 +21,24 @@ var geocodeAddress = (originalAddress, callback) => {
     });
 };
 
+var getTemperature = (argv, callback) => {
+    request({
+        url: `https://api.darksky.net/forecast/${argv.k}/${argv.lat},${argv.lon}`,
+        json: true
+    }, (error, response, body) => {
+        if (body.code === 400) {
+            callback('The given location (or time) is invalid.');
+        } else if (body === 'Forbidden') {
+            callback('User key is not authenticated.');
+        } else {
+            callback(undefined, {
+                temperature: body.currently.temperature
+            });
+        }
+    });
+};
+
 module.exports = {
-    geocodeAddress
+    geocodeAddress,
+    getTemperature
 };
